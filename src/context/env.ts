8806c724220987bc371c19307a99b421413d7856
AminPerez <joseamin1998@gmail.com>
2026-01-13 23:18:04 +0100

    REF: better typing and doc for the ENV context

--- src/context/env.ts
+++ src/context/env.ts
@@ -1,19 +1,80 @@
+import { IUltraPcConfig } from "@/types/TConfig";
 import { UltraContext, UltraFragment } from "@ultra-light";
 
-const ENV = UltraContext({
+interface IEnv {
+    /**
+     * Returns the next available item index.
+     */
+    itemIndex: number;
+    /**
+     * Returns true or false whether dark mode is enabled.
+     */
+    darkMode: boolean;
+    /**
+     * Returns true or false whether visual effects are enabled.
+     */
+    visualToggle: boolean;
+    /**
+     * Returns the speed of visual effects.
+     */
+    visualSpeed: number;
+    paused: boolean;
+    ignoreArpTraffic: boolean;
+    pcs: Record<string, string>;
+    routers: Record<string, string>;
+    switches: Record<string, string>;
+    servers: Record<string, string>;
+    $REQUEST_TIMEOUT: number;
+    $ARPENTRYTTL: number;
+    $MACENTRYTTL: number;
+    arpFlag: Record<string, boolean>;
+    icmpFlag: Record<string, boolean>;
+    dhcpDiscoverFlag: Record<string, boolean>;
+    dhcpRequestFlag: Record<string, boolean>;
+    dnsRequestFlag: Record<string, boolean>;
+    tcpSyncFlag: Record<string, boolean>;
+    traceFlag: Record<string, boolean>;
+    quickPingMode: boolean;
+    quickPingObject: Array<IUltraPcConfig>;
+    trace: Record<string, string>;
+    traceReturn: Record<string, string>;
+    nodes: Record<string, string>;
+    nodesNetmask: Record<string, string>;
+    nodesIp: Record<string, string>;
+    defaultNetwork: string;
+    buffer: Record<string, string>;
+    httpBuffer: Record<string, string>;
+    dhcpOfferBuffer: Record<string, string>;
+    tcpBuffer: Record<string, string>;
+    traceBuffer: Record<string, string>;
+    trafficBuffer: Array<string>;
+    routerChangesBuffer: Record<string, string>;
+    serverLeaseTimers: Record<string, ReturnType<typeof setTimeout>>;
+    clientLeaseTimers: Record<string, ReturnType<typeof setTimeout>>;
+    arpEntryTimers: Record<string, ReturnType<typeof setTimeout>>;
+    macEntryTimers: Record<string, ReturnType<typeof setTimeout>>;
+    dnsCacheTimers: Record<string, ReturnType<typeof setTimeout>>;
+    connTrack: Record<string, string>;
+}
+
+const ENV = UltraContext<IEnv>({
+
     itemIndex: 0,
     darkMode: false,
     visualToggle: true,
     visualSpeed: 300,
     paused: false,
     ignoreArpTraffic: false,
+
     pcs: {},
     routers: {},
     switches: {},
     servers: {},
+
     $REQUEST_TIMEOUT: 300,
     $ARPENTRYTTL: 120,
     $MACENTRYTTL: 120,
+
     arpFlag: {},
     icmpFlag: {},
     dhcpDiscoverFlag: {},
@@ -21,14 +82,18 @@ const ENV = UltraContext({
     dnsRequestFlag: {},
     tcpSyncFlag: {},
     traceFlag: {},
+
     quickPingMode: false,
-    quickPingObject: "",
+    quickPingObject: [],
+    
     trace: {},
     traceReturn: {},
+
     nodes: {},
     nodesNetmask: {},
     nodesIp: {},
     defaultNetwork: "",
+    
     buffer: {},
     httpBuffer: {},
     dhcpOfferBuffer: {},
@@ -36,12 +101,15 @@ const ENV = UltraContext({
     traceBuffer: {},
     trafficBuffer: [],
     routerChangesBuffer: {},
+
     serverLeaseTimers: {},
     clientLeaseTimers: {},
     arpEntryTimers: {},
     macEntryTimers: {},
     dnsCacheTimers: {},
+
     connTrack: {}
+
 });
 
 export { ENV as ENV };
