b34089f1b1e7b85fd1aee27faa41167b9b4da8aa
AminPerez <joseamin1998@gmail.com>
2026-01-18 11:58:22 +0100

    FEAT: add arp ignoring condition to the visualize method within ultraAnimations

--- src/hooks/ultraAnimations.ts
+++ src/hooks/ultraAnimations.ts
@@ -2,6 +2,7 @@ import { WORK_SPACE_CONTEXT as wsCtx } from "@/context/workspace-context";
 import { CANVAS_CONTEXT as csCtxd} from "@/components/core/svg-canvas";
 import { Packet } from "@/types/packets";
 import { IUltraAnimations } from "@/types/TConfig";
+import { ENV } from "@/context/env";
 
 export default function ultraAnimations(): IUltraAnimations {
     
@@ -13,9 +14,15 @@ export default function ultraAnimations(): IUltraAnimations {
 
         const originCoordinates = wsCtx.get().getCoordinatesByElementId(itemId1);
         const destinationCoordinates = wsCtx.get().getCoordinatesByElementId(itemId2);
-        const packetType = (packet.destinationMac === 'ff:ff:ff:ff:ff:ff') ? 'broadcast' : 'unicast';
+        const packetType = (packet.destinationMac === 'ff:ff:ff:ff:ff:ff') 
+        ? 'broadcast' 
+        : 'unicast';
 
         if (!originCoordinates || !destinationCoordinates) return;
+
+        if(packet.protocol === 'arp'){
+            if (ENV.get().ignoreArpTraffic === true) return;
+        }
 
         await csCtxd.get().createPacketAnimation(
             originCoordinates.x.toString(),
