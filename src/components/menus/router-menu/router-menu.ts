598f9f3d110263b63610db386da0d49de20ddf65
AminPerez <joseamin1998@gmail.com>
2026-01-18 13:39:00 +0100

    REF: better event handling for router menu / add menu frame

--- src/components/menus/router-menu/router-menu.ts
+++ src/components/menus/router-menu/router-menu.ts
@@ -1,10 +1,10 @@
 import { UltraActivity, UltraComponent, UltraLightElement, ultraState } from "@/ultra-light/ultra-light";
 import BasicSection from "./basic-section";
 import RoutingRulesSection from "./routing-rules-section";
-import { dragModal } from "@/utils/dragModal";
 import { ROUTER_MENU_CTX as rmCtx } from "@/context/modals";
 import { IRoutingRule } from "@/types/TConfig";
 import styles from './router-menu.module.css';
+import MenuFrame from "../menu-frame";
 
 export interface IRouterMenuFields {
     ip: string;
@@ -18,57 +18,65 @@ export function router_menu() {
     const [getIfaces, setIfaces, subscribeToIfaces] = ultraState<string[]>([]);
     const [getRoutingRules, setRoutingRules, subscribeToRoutingRules] = ultraState<IRoutingRule[]>([]);
     const [getPage, setPage, subscribeToPage] = ultraState<TPages>("basic");
+    const [getTitle, setTitle, subscribeToTitle] = ultraState<string>("");
+    const [getEventCleanup, setEventCleanup] = ultraState<(() => void) | null>(null);
 
     function onContextChange() {
         if (rmCtx.get().isVisible) {
-            dataDumpHandler();
-            window.addEventListener("keydown", onKeyDown);
+            onLoad();
         } else {
-            dataCleanupHandler();
-            window.removeEventListener("keydown", onKeyDown);
+            onCleanup();
         }
     }
 
-    function dataDumpHandler() {
+    function onLoad() {
         const routerAPI = rmCtx.get().routerElementAPI;
         if (!routerAPI) return;
         const ifaceIds = Object.keys(routerAPI.getIfaces());
         const currRules = routerAPI.routingRules();
         setIfaces(ifaceIds);
         setRoutingRules(currRules);
+        setTitle(routerAPI.properties().elementId);
+        setEventCleanup(onEvents());
     }
 
-    function dataCleanupHandler() {
+    function onCleanup() {
         setIfaces([]);
         setRoutingRules([]);
-    }
-
-    function titleHandler(self: HTMLElement) {
-        if (!rmCtx.get()?.isVisible) return;
-        const elementId = rmCtx.get().routerElementAPI?.properties().elementId;
-        if (!elementId) return;
-        self.innerHTML = elementId;
+        setTitle('');
+        getEventCleanup()?.();
+        setEventCleanup(null);
     }
 
     function onClose() {
-        dataCleanupHandler();
+        onCleanup();
         rmCtx.set({
             ...rmCtx.get(),
             "isVisible": false
         })
     }
 
-    function onKeyDown(event: KeyboardEvent) {
-        if (event.key === "Escape") {
-            onClose();
-        }
-    }
-
-    function onRoutingRulesChange(){
+    function onRoutingRulesChange() {
         const routerAPI = rmCtx.get().routerElementAPI;
         if (!routerAPI) return;
         const currRules = structuredClone(routerAPI.routingRules());
         setRoutingRules(currRules);
+    }
+
+    function onEvents() {
+
+        function onKeyDown(event: KeyboardEvent) {
+            if (event.key === "Escape") {
+                onClose();
+            }
+        }
+
+        window.addEventListener("keydown", onKeyDown);
+
+        return () => {
+            window.removeEventListener("keydown", onKeyDown);
+        }
+
     }
 
     return UltraActivity({
@@ -79,21 +87,11 @@ export function router_menu() {
 
             children: [
 
-                UltraComponent({
-
-                    component: `<div class="${styles['window-frame']}"></div>`,
-
-                    children: [
-                        UltraComponent({
-                            component: `<p class="${styles['frame-title']}"></p>`,
-                            trigger: [{ subscriber: rmCtx.subscribe, triggerFunction: titleHandler }]
-                        })
-                    ],
-
-                    eventHandler: {
-                        "mousedown": dragModal
-                    }
-
+                MenuFrame({
+                    initTitle: '',
+                    onClose,
+                    getTitle,
+                    titleSubscriber: subscribeToTitle
                 }),
 
                 UltraComponent({
@@ -105,11 +103,14 @@ export function router_menu() {
                         UltraComponent({
                             component: `<button class="btn-modern-blue selected">Basic</button>`,
                             eventHandler: { 'click': () => setPage('basic') },
-                            trigger: [{
-                                subscriber: subscribeToPage, triggerFunction: (self: UltraLightElement) => {
-                                    self.classList.toggle("selected", getPage() === "basic");
+                            trigger: [
+                                {
+                                    subscriber: subscribeToPage,
+                                    triggerFunction: (self: UltraLightElement) => {
+                                        self.classList.toggle("selected", getPage() === "basic");
+                                    }
                                 }
-                            }]
+                            ]
                         }),
 
                         UltraComponent({
