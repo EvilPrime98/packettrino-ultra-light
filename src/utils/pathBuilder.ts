d80a7c14eed8350d4afe7a68946c1cd5b705b1c9
AminPerez <joseamin1998@gmail.com>
2026-01-24 17:06:21 +0100

    REF: pathbuilder now returns an empty array when there's no input

--- src/utils/pathBuilder.ts
+++ src/utils/pathBuilder.ts
@@ -1,12 +1,11 @@
 import { TERMINAL_CONTEXT } from "@/context/terminal-context";
-import { EmptyArgumentError } from "@/errors";
 
 export default function pathBuilder(pathInput: string) {
 
     let path;
     const $PWD = TERMINAL_CONTEXT.get().pwd;
 
-    if (!pathInput) throw new EmptyArgumentError("Path is empty");
+    if (!pathInput) return [];
     
     if (pathInput.startsWith("/")) {
         path = pathInput.split("/");
