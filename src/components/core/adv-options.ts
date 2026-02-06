2130744aa81439cb567e9188b97b794b21254fd3
AminPerez <joseamin1998@gmail.com>
2026-02-06 09:49:11 +0100

    REF/FEAT: refactor props in AdvancedOptions component / created new component AdvancedOptionsDyn for dynamic options

--- src/components/core/adv-options.ts
+++ src/components/core/adv-options.ts
@@ -4,19 +4,17 @@ import { WORK_SPACE_CONTEXT as wCtx } from "@context/workspace-context";
 import { AdvancedOption } from "@/types/types";
 import styles from './adv-options.module.css';
 
-type Props = {
-    contextClickEvent: () => Event | null;
-    onClose?: () => void;
-    options: AdvancedOption[];
-    subscribeAdvOptionsState: (fn: (value: boolean) => void) => () => void;
-}
-
 export function AdvancedOptions({
     onClose,
     contextClickEvent,
     options,
     subscribeAdvOptionsState
-}: Props) {
+}: {
+    contextClickEvent: () => Event | null;
+    onClose?: () => void;
+    options: AdvancedOption[];
+    subscribeAdvOptionsState: (fn: (value: boolean) => void) => () => void;
+}) {
 
     const [
         getEventCleanup,
@@ -106,6 +104,150 @@ export function AdvancedOptions({
 
         trigger: [
             { subscriber: subscribeAdvOptionsState, triggerFunction: onVisibilityChange }
+        ],
+
+        eventHandler: {
+            'click': (event: Event) => event.stopPropagation()
+        }
+
+    })
+
+}
+
+export function AdvancedOptionsDyn({
+    onClose,
+    contextClickEvent,
+    options,
+    optionsSubscriber,
+    subscribeAdvOptionsState
+}: {
+    /**
+     * Stateful getter for the context click event.
+     * @returns 
+     */
+    contextClickEvent: () => Event | null;
+    /**
+     * This function is called when the user closes the context menu.
+     */
+    onClose?: () => void;
+    /**
+     * Stateful getter for the options.
+     */
+    options: () => AdvancedOption[];
+    /**
+     * Stateful subscriber for the options.
+     */
+    optionsSubscriber: (fn: (value: AdvancedOption[]) => void) => () => void;
+    /**
+     * Stateful subscriber for the visibility of the context menu.
+     */
+    subscribeAdvOptionsState: (fn: (value: boolean) => void) => () => void;
+}) {
+
+    const [
+        getEventCleanup,
+        setEventCleanup
+    ] = ultraState<(() => void) | null>(null);
+
+    function onVisibilityChange(
+        $modal: HTMLElement
+    ) {
+        if (getEventCleanup() !== null) {
+            getEventCleanup()?.();
+            setEventCleanup(null);
+            return;
+        }
+        setEventCleanup(onGlobalEvents());
+        const clickEvent = contextClickEvent();
+        if (!clickEvent || !(clickEvent instanceof MouseEvent)) {
+            onClose?.();
+            return;
+        }
+        onModalAdjustments($modal, clickEvent);
+    }
+
+    function onModalAdjustments(
+        $modal: HTMLElement,
+        clickEvent: MouseEvent
+    ) {
+
+        wCtx.get().measureBoard();
+        const { boardHeight, boardWidth, boardRect } = wCtx.get().boardProperties;
+        const workRectLeft = boardRect?.left || 0;
+        const workRectTop = boardRect?.top || 0;
+        const x = clickEvent.clientX - workRectLeft;
+        const y = clickEvent.clientY - workRectTop;
+
+        if (y < boardHeight / 2 && x < boardWidth / 2) {
+            $modal.style.top = "30%";
+            $modal.style.bottom = "";
+            $modal.style.left = "50%";
+            $modal.style.right = "";
+        } else if (y < boardHeight / 2 && x >= boardWidth / 2) {
+            $modal.style.top = "30%";
+            $modal.style.bottom = "";
+            $modal.style.left = "";
+            $modal.style.right = "30%";
+        } else if (y >= boardHeight / 2 && x < boardWidth / 2) {
+            $modal.style.top = "";
+            $modal.style.bottom = "30%";
+            $modal.style.left = "30%";
+            $modal.style.right = "";
+        } else {
+            $modal.style.top = "";
+            $modal.style.bottom = "30%";
+            $modal.style.left = "";
+            $modal.style.right = "30%";
+        }
+    }
+
+    function onGlobalEvents() {
+        function onClickOutside(event: Event) {
+            const $el = event.target as HTMLElement;
+            if (!$el.closest('.advanced-options-modal')) onClose?.();
+        }
+        window.addEventListener('click', onClickOutside);
+        return () => {
+            window.removeEventListener('click', onClickOutside);
+        }
+    }
+
+    function onOptionsChange(
+        $div: HTMLElement
+    ){
+        $div.innerHTML = '';
+        $div.append(...options().map(option =>
+            Option({
+                message: option.message,
+                callback: (event: Event | null) => {
+                    option.callback(event);
+                    onClose?.();
+                }
+            })
+        ));
+    }
+
+    return UltraComponent({
+
+        component: '<div></div>',
+
+        className: ['modal', styles['advanced-options-modal']],
+
+        children: [
+            ...options().map(option =>
+                Option({
+                    message: option.message,
+                    callback: (event: Event | null) => {
+                        option.callback(event);
+                        onClose?.();
+                    }
+                })
+            )
+        ],
+
+        trigger: [
+            { subscriber: subscribeAdvOptionsState, triggerFunction: onVisibilityChange },
+            { subscriber: optionsSubscriber, triggerFunction: onOptionsChange, defer: true }
         ],
 
         eventHandler: {
