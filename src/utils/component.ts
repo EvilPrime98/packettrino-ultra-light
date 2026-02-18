a9d6b1d4124824bb393293f588799926c02006d0
AminPerez <joseamin1998@gmail.com>
2026-02-18 23:07:52 +0100

    REF/FEAT: added typing to createFileSystem function / created a helper function for deleting/cleanup a network element.

--- src/utils/component.ts
+++ src/utils/component.ts
@@ -5,12 +5,17 @@ import DhcpServer from "@/components/network-elements/dhcp-server";
 import TextObject from "@/components/tools/notes/note";
 import { ENV } from "@/context/env-context";
 import { TElementFactory, TCreatableElement } from "@/types/TWorkSpace";
+import { IPTTFolder } from "./pttFileSystem";
+import { UltraLightElement } from "@/ultra-light/types";
+import { TLayer2Config, TLayer3Config } from "@/types/TConfig";
+import { isLayer3 } from "@/types/typeguards";
+import { TOASTER_CONTEXT } from "@/context/toaster-context";
 
 /**
- * Function that creates a filesystem object for any network element.
- * @returns 
+ * Creates a basic filesystem object for any network element.
+ * @returns a filesystem object.
  */
-export function createFilesystem() {
+export function createFilesystem(): Record<string, IPTTFolder> {
 
     return {
         "/": {
@@ -43,7 +48,7 @@ export function getNextElementId(itemType: string): string {
     const foo = ENV.get();
     const currentIndex = foo["itemIndex"];
     const newId = `${itemType}-${currentIndex}`;
-    ENV.set({ 
+    ENV.set({
         ...foo,
         itemIndex: currentIndex + 1
     });
@@ -57,7 +62,7 @@ export function getNextElementId(itemType: string): string {
  * @returns 
  */
 export function createElementMap(
-    x: number, 
+    x: number,
     y: number
 ): Record<TCreatableElement, TElementFactory> {
     return (
@@ -69,4 +74,51 @@ export function createElementMap(
             "dhcp-server": (id) => DhcpServer({ x, y, id })
         }
     );
+}
+
+/**
+ * Cleans up and deletes a network element.
+ * @param element UltraLight Element representing the network element.
+ * @param elementAPI Element API of the network element.
+ * @param notification Whether to show an error notification or not.
+ */
+export function deleteElement(
+    element: UltraLightElement,
+    elementAPI: TLayer2Config | TLayer3Config,
+    notification: boolean = false
+) {
+
+    try {
+
+        if (isLayer3(elementAPI)) {
+            const ifaces = elementAPI.getIfaces();
+            for (const ifaceId of Object.keys(ifaces)) {
+                const iface = ifaces[ifaceId];
+                if (iface.connection.api !== null) {
+                    throw new Error("Cannot delete an element that is connected to another element.");
+                }
+            }
+        } else {
+            const connections = elementAPI.properties().connections;
+            for (const connection of connections) {
+                if (connection.api !== null) {
+                    throw new Error("Cannot delete an element that is connected to another element.");
+                }
+            }
+        }
+
+        element._cleanup?.();
+        element.remove();
+
+    }catch (error) {
+
+        if (notification) {
+            TOASTER_CONTEXT.get().createNotification(
+                (error instanceof Error) ? error.message : 'Unknown error',
+                'error'
+            )
+        }
+
+    }
+
 }
