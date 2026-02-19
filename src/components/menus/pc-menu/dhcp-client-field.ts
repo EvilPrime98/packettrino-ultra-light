dbb9a2041ffed1644b1358ac82e933a9178d675b
AminPerez <joseamin1998@gmail.com>
2026-02-20 00:17:34 +0100

    FEAT: pc-menu -> shows lease time on real time

--- src/components/menus/pc-menu/dhcp-client-field.ts
+++ src/components/menus/pc-menu/dhcp-client-field.ts
@@ -1,5 +1,5 @@
 import { FormInput } from "@/components/core/form-input"
-import { UltraActivity, UltraComponent, ultraState } from "@/ultra-light/ultra-light";
+import { UltraActivity, ultraState } from "@/ultra-light/ultra-light";
 import styles from './pc-menu.module.css'
 import { PC_MENU_CTX as pmCtx } from "@/context/pc-menu-context";
 import { hasDHCPClient } from "@/types/typeguards";
@@ -33,32 +33,45 @@ export function DhcpClientField({
     const [getVisible, setVisible, subscribeToVisible] = ultraState<boolean>(false);
     const [isDhcpEnabled, setDhcpEnabled, subscribeToDhcpEnabled] = ultraState<boolean>(false);
     const [isSearching, setSearching, subscribeToSearching] = ultraState<boolean>(false);
+    const [getLeaseTime, setLeaseTime, subscribeToLeaseTime] = ultraState<number>(0);
 
     /**
      * Executes when:
      * - the PC Menu context changes
-     * - the selected interface changes
      * @returns 
      */
-    function onLoad() {
+    function onContextChange() {
         if (!pmCtx.get().isVisible) {
             onCleanup();
             return;
         }
+    }
+
+    /**
+     * Executes: 
+     * - on initial load
+     * - when the selected interface changes.
+     */
+    function dhcpClientHandler(){
         const elementAPI = pmCtx.get().pcElementAPI;
         if (!elementAPI) return;
         if (!hasDHCPClient(elementAPI)) {
             setVisible(false);
             setDhcpEnabled(false);
             blockFields(false);
-        }else{
+        } else {
             setVisible(true);
             const dhcpIfaces = elementAPI.dhcpClient.getDhcpIfaces();
             const currIface = fields.iface.get();
             if (dhcpIfaces.includes(currIface)) {
                 setDhcpEnabled(true);
                 blockFields(true);
-            }else {
+                const lease = elementAPI.dhcpClient.getLeases()
+                .find(lease => lease.ifaceId === currIface);
+                if (lease) {
+                    setLeaseTime(lease.leasetime);
+                }
+            } else {
                 setDhcpEnabled(false);
                 blockFields(false);
             }
@@ -79,12 +92,42 @@ export function DhcpClientField({
 
     /**
      * Starts a DHCP (discover) process on the selected interface.
+     * In case the interfaces gets a lease, it updates the lease time.
      * @returns 
      */
     async function onSearch() {
+        
         setSearching(true);
         await onDhcpProcess();
         setSearching(false);
+        
+        const elementAPI = pmCtx.get().pcElementAPI;
+        if (!elementAPI) return;
+        
+        if (!hasDHCPClient(elementAPI)) return;
+        
+        const leases = elementAPI.dhcpClient.getLeases();
+        const lease = leases.find(lease => lease.ifaceId === fields.iface.get());
+
+        if (lease) {
+            setLeaseTime(lease.leasetime);
+            elementAPI.dhcpClient.subscribeToLeases(() => {
+                setLeaseTime(lease.leasetime);
+            })
+        }
+
+    }
+
+    /**
+     * Executes when the lease time (if there is one) changes.
+     * @param self 
+     */
+    function updateLeaseTime(
+        self: HTMLElement
+    ){
+        const $span = self as HTMLSpanElement;
+        const leaseTime = getLeaseTime();
+        $span.innerText = `${leaseTime}s`;
     }
 
     function onCleanup() {
@@ -99,59 +142,50 @@ export function DhcpClientField({
             subscriber: subscribeToVisible
         },
 
-        component: UltraComponent({
+        component: FormInput({
 
-            component: '<div></div>',
-
+            id: 'dhcp-client-field',
+            name: 'dhcp-client-field',
+            label: 'DHCP Client',
+            type: 'checkbox',
             className: [styles['dhcp-client-field']],
+            getValue: isDhcpEnabled,
+            changeSubscriber: subscribeToDhcpEnabled,
+            onChange: onChange,
 
-            children: [
+            adChildren: [
 
-                FormInput({
-
-                    id: 'dhcp-client-field',
-                    name: 'dhcp-client-field',
-                    label: 'DHCP Client',
-                    type: 'checkbox',
-                    className: [styles['form-item']],
-                    getValue: isDhcpEnabled,
-                    changeSubscriber: subscribeToDhcpEnabled,
-                    onChange: onChange,
-
-                    adChildren: [
+                UltraActivity({
+                    mode: { state: isDhcpEnabled, subscriber: subscribeToDhcpEnabled },
+                    type: 'visibility',
+                    component: '<button type="button"></button>',
+                    className: ['btn-modern-blue', 'small'],
+                    eventHandler: { click: onSearch },
+                    trigger: [
+                        {
+                            subscriber: subscribeToSearching,
+                            triggerFunction: ($button: HTMLElement) => {
+                                ($button as HTMLButtonElement).disabled = isSearching();
+                            }
+                        }
+                    ],
+                    children: [
 
                         UltraActivity({
-                            mode: { state: isDhcpEnabled, subscriber: subscribeToDhcpEnabled },
-                            type: 'visibility',
-                            component: '<button type="button"></button>',
-                            className: ['btn-modern-blue', 'small'],
-                            eventHandler: { click: onSearch },
+                            mode: { state: () => !isSearching(), subscriber: subscribeToSearching },
+                            component: '<span>Search</span>',
                             trigger: [
-                                {
-                                    subscriber: subscribeToSearching,
-                                    triggerFunction: ($button: HTMLElement) => {
-                                        ($button as HTMLButtonElement).disabled = isSearching();
-                                    }
-                                }
-                            ],
-                            children: [
-
-                                UltraActivity({
-                                    mode: { state: () => !isSearching(), subscriber: subscribeToSearching },
-                                    component: '<span>Search</span>',
-                                }),
-
-                                UltraActivity({
-                                    mode: { state: isSearching, subscriber: subscribeToSearching },
-                                    component: Loader({ size: 20, border: 3 }),
-                                })
-
+                                { subscriber: subscribeToLeaseTime, triggerFunction: updateLeaseTime }
                             ]
+                        }),
+
+                        UltraActivity({
+                            mode: { state: isSearching, subscriber: subscribeToSearching },
+                            component: Loader({ size: 20, border: 3 }),
                         })
 
                     ]
-
-                }),
+                })
 
             ]
 
@@ -160,12 +194,12 @@ export function DhcpClientField({
         trigger: [
             {
                 subscriber: pmCtx.subscribe,
-                triggerFunction: onLoad,
+                triggerFunction: onContextChange,
                 defer: true
             },
-            { 
-                subscriber: fields.iface.subscribe, 
-                triggerFunction: onLoad
+            {
+                subscriber: fields.iface.subscribe,
+                triggerFunction: dhcpClientHandler
             }
         ]
 
